19f0ec585a7925be52b1de13a0acc5ee5ca7cced
LAOUZAILina <125370735+LAOUZAILina@users.noreply.github.com>
2023-12-15 13:40:48 +0100

    app created
    it is our react app with tailwind installed

---
+++ paper_website/src/App.js
@@ -0,0 +1,14 @@
+//import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    
+        <p className='text-6xl'>
+          Linoooooooo is hereeeeeeeeeee .
+        </p>
+        
+  );
+}
+
+export default App;
