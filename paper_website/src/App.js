43826a5643e98b1bd8b0c043a6c59034cdd19af2
amourayusra <96579973+amourayusra@users.noreply.github.com>
2023-12-18 11:37:18 +0100

    commited

--- paper_website/src/App.js
+++ paper_website/src/App.js
@@ -1,13 +1,14 @@
-import './App.css';
-import NavBar from './Components.jsx/navbar';
-import Card from './Components.jsx/card';
+import "./App.css";
+import NavBar from "./Components.jsx/navbar";
+import Card from "./Components.jsx/card";
+import Details from "./Components.jsx/details";
 
 function App() {
   return (
-    <div className='bg-[#E7F1FF] min-h-screen p-0 m-0'>
+    <div className="bg-[#E7F1FF] min-h-screen p-0 m-0">
       <div id="root">
         <NavBar />
-        
+        <Details />
       </div>
     </div>
   );
---
+++ paper_website/src/Components.jsx/details.jsx
@@ -0,0 +1,56 @@
+function Details() {
+  return (
+    <>
+      <div className="bg-white rounded-[24.44px]">
+        <div className="font-bold text-[#005BC5] pl-[32.89px] pt-[39.11px] pb-[52.89px] text-[28px]">
+          Semantic Analysis And Classifications Of E-mails <br />
+          Throught Informative Selection
+        </div>
+        <div className="px-[61px]">
+          <div className=" font-bold">Title</div>
+          <div className="pb-[14px]">
+            Semantic analysis and classifications of E-mails throught
+            informative Selection
+          </div>
+          <div className=" font-bold">Resume</div>
+          <div className="pb-[14px]">
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book. It has survived not
+            only five centuries, but also the leap into electronic typesetting,
+            remaining essentially unchanged. It was popularised in the 1960s
+            with the release of Letraset sheets containing Lorem Ipsum passages,
+            and more recently with desktop publishing software like Aldus
+            PageMaker including versions of Lorem Ipsum...
+          </div>
+          <div className="font-bold">Writer(s) :</div>
+          <div className="pb-[14px]">Yusra , Lina</div>
+          <div className=" font-bold">Institution(s) : </div>
+          <div className="pb-[14px]">ESI, Poly</div>
+          <div className="font-bold">Références bibliographiques : </div>
+          <div className="pb-[14px]">ESI, Poly</div>
+          <div className="font-bold">Integral Text</div>
+          <div className="pb-[14px]">
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book. It has survived not
+            only five centuries, but also the leap into electronic typesetting,
+            remaining essentially unchanged. It was popularised in the 1960s
+            with the release of Letraset sheets containing Lorem Ipsum passages,
+            and more recently with desktop publishing software like Aldus
+            PageMaker including versions of Lorem Ipsum lorem Ipsum is simply
+            dummy text of the printing and typesetting industry. Lorem Ipsum has
+            been the industry's ldus PageMaker including versions of Lorem Ipsum
+          </div>
+          <div className="flex items-center justify-center w-[612px] h-[68px] rounded-[17px] mx-[280px] mt-[122px] mb-[130px] bg-[#1B9DF0] text-[20px] text-white font-semibold">
+            Link to pdf
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Details;
