b6fbe964eb81c0e8709db4f2bd118349486162aa
LAOUZAILina <125370735+LAOUZAILina@users.noreply.github.com>
2023-12-27 16:01:08 +0100

    commit
    this time i added the section where we see the moderators infos

--- paper_website/src/Components.jsx/DashboardModer.jsx
+++ paper_website/src/Components.jsx/DashboardModer.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import DataTable from 'react-data-table-component';
+import Noselected from "../assets/Noselected.svg";
 
 
 function DashboardModer() {
@@ -19,6 +20,11 @@ function DashboardModer() {
           name: "password",
           selector: row => row.password
         },
+        {
+            name: "ID",
+            selector: row => row.id
+          },
+          
       ];
         const data = [
           {
@@ -26,6 +32,7 @@ function DashboardModer() {
             name: 'Alice',
             email: 'alice@example.com',
             password: 'alice123'
+           
           },
           {
             id: 2,
@@ -74,20 +81,87 @@ function DashboardModer() {
             name: 'Eva',
             email: 'eva@example.com',
             password: 'eva543'
+          },
+          {
+            id: 10,
+            name: 'Bob',
+            email: 'bob@example.com',
+            password: 'bob456'
+          },
+          {
+            id: 11,
+            name: 'Charlie',
+            email: 'charlie@example.com',
+            password: 'charlie789'
+          },
+          {
+            id: 12,
+            name: 'David',
+            email: 'david@example.com',
+            password: 'david987'
+          },
+          {
+            id: 13,
+            name: 'Eva',
+            email: 'eva@example.com',
+            password: 'eva543'
+          },
+          {
+            id: 14,
+            name: 'Bob',
+            email: 'bob@example.com',
+            password: 'bob456'
+          },
+          {
+            id: 15,
+            name: 'Charlie',
+            email: 'charlie@example.com',
+            password: 'charlie789'
+          },
+          {
+            id: 16,
+            name: 'David',
+            email: 'david@example.com',
+            password: 'david987'
+          },
+          {
+            id: 17,
+            name: 'Eva',
+            email: 'eva@example.com',
+            password: 'eva543'
           }
         ];
+       
+        
   return (
-    <div className=' rounded-xl m-5'>
+    <div className=' rounded-[30px] m-5 mt-10 bg-white p-8'>
             {/* <h1>Moderator Registration</h1> */}
             {/* <ModerInfos/> */}
-            <DataTable 
-            columns={columns}
+            <div className='flex gap-5 rounded-[20px] '>
+            <div className='flex-grow border-[5px] border-blue-100 rounded-[20px]'>
+            <DataTable className=' border-blue-100 rounded-[20px]'
+            columns={columns }
               data={data}
               selectableRows
-              fixedHeader>
+              fixedHeader
+              pagination
+              paginationPerPage={5} //the default number of rows imitially
+              paginationRowsPerPageOptions={[5,7,10]}
+        
+              >
 
 
             </DataTable>
+            </div>
+            <div className='bg-white border-[5px] border-blue-100 rounded-[20px]'> 
+              <div className=' items-center'>
+                 <img className='px-[100px] pt-[100px]' src={Noselected} alt="No moderator selected" />
+                 <div className="text-center text-blue-200 text-sm font-medium tracking-tight">Select a moderator <br/>to see more more informations</div>
+             </div>
+            </div>
+                
+            </div>
+            
             
          </div>
   )
