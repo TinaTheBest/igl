84320c6d9a4f9184ca965a654ea43a62b06ed299
LAOUZAILina <125370735+LAOUZAILina@users.noreply.github.com>
2023-12-29 00:26:32 +0100

    Update App.js

--- paper_website/src/App.js
+++ paper_website/src/App.js
@@ -1,37 +1,43 @@
-import React, { useState } from 'react';
 import "./App.css";
 import NavBar from "./Components.jsx/navbar";
-import SearchBar from "./Components.jsx/SearchBar"; 
-import FilterButton from "./Components.jsx/Filter"; 
+import CardMod from "./Components.jsx/cardMod";
+import Card from "./Components.jsx/card";
+import Details from "./Components.jsx/details";
 import TagsInput from "./Components.jsx/TagsInput";
-import ExtendedFilter from "./Components.jsx/ExtendedFilter";
-
+import x from "./assets/x.svg";
+import DetailsMod from "./Components.jsx/detailsMod";
+import ModifyMod from "./Components.jsx/modifyMod";
+import ModifyModInput from "./Components.jsx/modifyModInput";
+import NavBarMod from "./Components.jsx/navbarMod";
+import Login from "./Components.jsx/login";
+import SignUp from "./Components.jsx/signup";
+import ModeratorFirstPage from "./Pages/Moderator/ModeratorFirstPage";
+import ModeratorModify from "./Pages/Moderator/ModeratorModify";
+import UserFirstPage from "./Pages/User/UserFirstPage";
+import UserFav from "./Pages/User/UserFav";
+import UserDetails from "./Pages/User/UserDetails";
+import UserFilters from "./Pages/User/UserFilters";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import ModeratorDetails from "./Pages/Moderator/ModeratorDetails";
+import PageLogin from "./Pages/PageLogin";
+import PageSignUp from "./Pages/PageSignUp";
+import AdminFirstPage from "./Pages/Administrator/AdminFirstPage";
+import DashboardModer from "./Components.jsx/DashboardModer";
 function App() {
-  const [isExtendedFilterVisible, setExtendedFilterVisibility] = useState(false);
-
-  const handleSearch = (tags) => {
-    // Implement your search logic here using the 'tags' object
-    console.log('Searching with tags:', tags);
-  };
-
-  const toggleExtendedFilter = () => {
-    setExtendedFilterVisibility(!isExtendedFilterVisible);
-  };
-
   return (
-    <div className="bg-[#E7F1FF] min-h-screen p-0 m-0">
+    <div className="bg-[#E7F1FF] min-h-screen p-0 m-0 relative">
       <div id="root">
-        <NavBar />
-        <div className="flex items-start sm:space-x-4 w-full h-[56.44px] sm:px-[40.8px] px-[20.8px] justify-between gap-[33px] ">
-          <div className="flex items-center justify-center  mb-4 sm:mb-0">
-            {isExtendedFilterVisible ? (
-              <ExtendedFilter onSearch={handleSearch} />
-            ) : (
-              <FilterButton onClick={toggleExtendedFilter} />
-            )}
-          </div>
-          <SearchBar />
-        </div>
+        {/* <DashboardModer/> */}
+        <Router>
+          <Routes>
+           
+            <Route
+              path="/AdminFirstPage"
+              element={<AdminFirstPage />}
+            ></Route>
+           
+          </Routes>
+        </Router>
       </div>
     </div>
   );
