db2c675790e18e769cc6c5c9e9164dbfafa2fe37
TinaTheBest <131246422+TinaTheBest@users.noreply.github.com>
2023-12-29 19:21:51 +0100

    tinn

--- back_end/routes/User.py
+++ back_end/routes/User.py
@@ -7,15 +7,16 @@ user = Blueprint('users', __name__)
 
 @user.route('/get_all_data', methods=['GET'])
 def get_all_data():
-    # Define your Elasticsearch index
-    index_name = "article_valide"
-    if not es.indices.exists(index=index_name):
-        es.indices.create(index=index_name)
-    # Perform a match_all query to get all documents
-    results = es.search(index=index_name, body={"query": {"match_all": {}}})
+    # Check if the validated articles index exists
+    valid_index_name = "article_valide"
+    if not es.indices.exists(index=valid_index_name):
+        es.indices.create(index=valid_index_name)
+
+    # Perform a match_all query to get all validated articles
+    results = es.search(index=valid_index_name, body={"query": {"match_all": {}}})
 
     # Extract relevant information from the search results
     hits = results['hits']['hits']
-    response_data = [{'id': hit['_id'], 'source': hit['_source']} for hit in hits]
+    
+    return jsonify(hits)
 
-    return jsonify(response_data)
