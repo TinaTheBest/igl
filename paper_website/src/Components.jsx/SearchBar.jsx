46fcac32f907c3a9fe20a95e000946990321e729
LAOUZAILina <125370735+LAOUZAILina@users.noreply.github.com>
2024-02-01 15:22:45 +0100

    search when pressing enter

--- paper_website/src/Components.jsx/SearchBar.jsx
+++ paper_website/src/Components.jsx/SearchBar.jsx
@@ -1,8 +1,8 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 import axios from "axios";
-const SearchBar = ({ onSearch }) => {
+const  SearchBar = ({ onSearch }) => {
   // State to hold the search term
-  const [searchTerm, setSearchTerm] = useState("");
+  const [searchTerm, setSearchTerm] = useState('');
 
   // Function to handle search changes
   const handleSearchChange = (e) => {
@@ -10,37 +10,47 @@ const SearchBar = ({ onSearch }) => {
     console.log(searchTerm);
     recherchegenerale();
   };
-
+  // Function to handle search when Enter key is pressed
+  const handleKeyPress = (e) => {
+    if (e.key === 'Enter') 
+    {
+      recherchegenerale();
+    }
+  };
+  
   const recherchegenerale = async () => {
     try {
       const response = await axios.post(
         "http://127.0.0.1:5000/recherche/resultat",
-        { search_term: searchTerm }
+        { "search_term": searchTerm }
       );
-
-      console.log("cc", response);
+      
+      console.log("cc",response);
       onSearch(response.data);
+
     } catch (error) {
-      console.error("Error adding moderateur :", error);
+      console.error("Error fla recherche :", error);
     }
   };
-
+  
   return (
     <div className="flex w-full h-[49.78px] gap-[17.78px] justify-between items-center space-x-2">
       <input
         type="text"
         value={searchTerm}
         onChange={handleSearchChange}
+        onKeyPress={handleKeyPress} // Call handleKeyPress on key press
         className="w-full h-[49.78px] px-[17.78px] py-[14.22px] bg-white rounded-[17.78px] justify-between items-center inline-flex focus:outline-none "
         placeholder="Search..."
       />
-      <button
-        className=" hidden sm:flex justify-center items-center sm:gap-8.889px sm:text-white sm:text-[21.333px] sm:bg-[#1B9DF0] sm:h-[49.778px] sm:w-[184px] sm:rounded-[17.778px] sm:px-[21.3px] sm:py-[14.222] font-medium font-['DM Sans'] shadow-lg"
-        onClick={recherchegenerale}
+      <button className=" hidden sm:flex justify-center items-center sm:gap-8.889px sm:text-white sm:text-[21.333px] sm:bg-[#1B9DF0] sm:h-[49.778px] sm:w-[184px] sm:rounded-[17.778px] sm:px-[21.3px] sm:py-[14.222] font-medium font-['DM Sans'] shadow-lg"
+      onClick={recherchegenerale}
       >
         Search
       </button>
     </div>
+
+    
   );
 };
 
