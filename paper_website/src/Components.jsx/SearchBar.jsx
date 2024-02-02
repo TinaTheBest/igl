8377453490dde8d624fb5c93065af2c540c5bc2e
LAOUZAILina <125370735+LAOUZAILina@users.noreply.github.com>
2024-02-02 19:42:24 +0100

    search update

--- paper_website/src/Components.jsx/SearchBar.jsx
+++ paper_website/src/Components.jsx/SearchBar.jsx
@@ -41,16 +41,16 @@ const  SearchBar = ({ onSearch }) => {
   };
   
   return (
-    <div className="flex w-full h-[49.78px] gap-[17.78px] justify-between items-center space-x-2">
+    <div className="flex w-full h-[49.78px] gap-[1px] justify-between items-center space-x-2 ">
       <input
         type="text"
         value={searchTerm}
         onChange={handleSearchChange}
         onKeyPress={handleKeyPress} // Call handleKeyPress on key press
-        className="w-full h-[49.78px] px-[17.78px] py-[14.22px] bg-white rounded-[17.78px] justify-between items-center inline-flex focus:outline-none "
+        className="w-full h-[49.78px] px-[17.78px] py-[14.22px] bg-white rounded-[17.78px] justify-between items-center inline-flex focus:outline-none shadow-lg"
         placeholder="Search..."
       />
-      <button className=" hidden sm:flex justify-center items-center sm:gap-8.889px sm:text-white sm:text-[21.333px] sm:bg-[#1B9DF0] sm:h-[49.778px] sm:w-[184px] sm:rounded-[17.778px] sm:px-[21.3px] sm:py-[14.222] font-medium font-['DM Sans'] shadow-lg"
+      <button className=" hidden sm:flex justify-center items-center sm:gap-8.889px sm:text-white sm:text-[21.333px] sm:bg-[#1B9DF0] sm:h-[47.778px] sm:w-[184px] sm:rounded-[17.778px] sm:px-[21.3px] sm:py-[14.222] font-medium font-['DM Sans'] shadow-lg"
       onClick={recherchegenerale}
       >
         Search
